c9a884f5e8ced7e5854b2b5b335ee31c5a9a1ceb
KatharinaGitStar <168305372+KatharinaGitStar@users.noreply.github.com>
2024-05-05 23:08:58 +0200

    Update Artist.java
    updates of Artist.java after testing the code

--- Artist.java
+++ Artist.java
@@ -1,12 +1,24 @@
+package MusicLandscape.entities;
+
 public class Artist {
     private String name;
 
-    public Artist(String name){
+
+    // Constructor with a default name
+    public Artist() {
+        this.name = null;
+    }
+
+    public Artist(String name) {
         this.name = name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name == null || name.trim().isEmpty()) {
+            this.name = null;
+        } else {
+            this.name = name;
+        }
     }
 
     public String getName() {
