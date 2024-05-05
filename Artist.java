9a528dc492fbbdfdbc7d8f5a001970a409f2c84b
KatharinaGitStar <168305372+KatharinaGitStar@users.noreply.github.com>
2024-05-05 16:47:10 +0200

    implementation of methods for class Track including encapsulation implementation of encapsulation in class Artist

--- Artist.java
+++ Artist.java
@@ -1,6 +1,10 @@
 public class Artist {
     private String name;
 
+    public Artist(String name){
+        this.name = name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
--- Track.java
+++ Track.java
@@ -4,5 +4,62 @@ public class Track {
     private Artist writer;
     private Artist performer;
     private int year;
+
+    public Track(String title, int duration, Artist writer, Artist performer, int year) {
+        this.title = title;
+        this.duration = duration;
+        this.writer = writer;
+        this.performer = performer;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Artist getWriter() {
+        return writer;
+    }
+
+    public void setWriter(Artist writer) {
+        this.writer = writer;
+    }
+
+    public Artist getPerformer() {
+        return performer;
+    }
+
+    public void setPerformer(Artist performer) {
+        this.performer = performer;
+    }
+
+    public boolean writerIsKnown() {
+        return writer != null;
+    }
+
+    public String getString() {
+        //example implementation, not final version
+        return "Title: " + title + ", Duration: " + duration;
+    }
 }
 
